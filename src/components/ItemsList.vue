d16c679621708253283754cc96203f9172a1958f
Mateus da Silva <mateus.wtv@gmail.com>
2019-11-03 13:34:25 -0300

    add ItemsList component
    add title style

---
+++ src/components/ItemsList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="items">
+    <p class="items_lines"><span>Veja como será apresentado ao cliente</span></p>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+.items {
+  position: relative;
+  width: 100%;
+}
+.items_lines {
+  position: relative;
+  line-height: 0.5;
+  text-align: center;
+  color: #A03400;
+  font-weight: bold;
+  font-style: italic;
+  font-size: 29px;
+}
+.items_lines span {
+  display: inline-block;
+  position: relative;
+}
+.items_lines span::before, .items_lines span::after {
+  content: "";
+  position: absolute;
+  height: 5px;
+  border-bottom: 1px solid #E43636;
+  top: 0;
+  width:100%;
+}
+.items_lines span::before {
+  right: 100%;
+  margin-right: 5rem;
+}
+.items_lines span::after {
+  left: 100%;
+  margin-left: 5rem;
+}
+
+</style>

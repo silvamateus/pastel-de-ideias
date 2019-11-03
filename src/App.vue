be5c24639ad58eca750fbdaf7f326921e7bded8c
Mateus da Silva <mateus.wtv@gmail.com>
2019-11-03 13:35:02 -0300

    add ItemsList component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Home/>
+    <items-list/>
   </div>
 </template>
 
 <script>
 import Home from './components/Home.vue';
+import ItemsList from './components/ItemsList'
 
 export default {
   name: 'app',
   components: {
     Home,
+    ItemsList
   }
 }
 </script>
@@ -20,10 +23,11 @@ export default {
 @import "../node_modules/normalize.css/normalize.css";
 
 /*PROJECT*/
+
 #app {
   background-image: url('./assets/pattern.png');
   background-repeat: repeat;
   min-height: 100vh;
-  z-index: -2;
 }
+
 </style>

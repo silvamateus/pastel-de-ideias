c612ca20bd7152658ddc3ab807e152204e175dbd
Mateus da Silva <mateus.wtv@gmail.com>
2019-11-04 15:29:35 -0300

    add description v-model

--- src/components/AddItem.vue
+++ src/components/AddItem.vue
@@ -4,21 +4,21 @@
       <div class="block_header">
         <p>Monte aqui o seu cardápio. O que está esperando?</p>
       </div>
-      <form class="block_form" v-on:submit.prevent >
+      <form class="block_form" v-on:submit.prevent="onSubmit" >
         <div class="form_header">
           <input v-model="title" type="text" placeholder="Título do pedido"/>
           <input v-model="taste" type="text" placeholder="Sabor"/>
           <input v-model="value" type="text" placeholder="R$"/>
         </div>
-        <textarea placeholder="Descrição" class="block_form_input--size-big"/>
+        <textarea v-model="description" placeholder="Descrição" class="block_form_input--size-big"/>
         <label class="block_form_file block_form_input--size-big" for="img-upload">
           <i class="far fa-image image-upload"></i>
           <p>Jogue aqui o arquivo de imagem do seu pastel ou clique para localizar a pasta</p>
         </label>
         <input type="file" id="img-upload" accept="image/*" @change="uploadImage($event)"/>
         <div class="block_form_buttons">
-          <button class="clear">LIMPAR</button>
-          <button class="register" @click="onCLick(title, taste, value)" >CADASTRAR</button>
+          <button class="clear" @click="onClear()">LIMPAR</button>
+          <button class="register" type="submit">CADASTRAR</button>
         </div>
       </form>
     </div>
